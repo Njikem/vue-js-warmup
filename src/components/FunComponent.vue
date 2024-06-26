2beed7864de9a6941815b88c0b200bc3d3f63a28
Njikem <123434988+Njikem@users.noreply.github.com>
2024-06-26 15:32:31 -0600

    button, event, text

---
+++ src/components/FunComponent.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+     <h4>This is fun</h4>
+
+
+   <div>
+      <button @click="buttonClicked($event)">Clicked</button>
+   </div>
+
+   <div v-for="(secret, i) in secret" :key="i" @click="textColor($event)">
+
+     <p>{{secret.name}}</p>
+
+
+   </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'FunComponent',
+    components: {
+
+    },
+
+    data() {
+        return {
+
+           secret: [
+            {
+                name: 'Johny', 
+
+            },
+
+            {
+                name: 'Mauby', 
+
+            },
+
+            {
+                name: 'Rose', 
+
+            },
+
+            {
+                name: 'Anas', 
+
+            }
+        
+           ]
+          
+        }
+    },
+
+
+    methods: {
+        buttonClicked(event) {
+            console.log('buttonClicked', event);
+
+            event.target.style.backgroundColor = 'red';
+            event.target.style.color = 'green';
+            
+            document.body.style.backgroundColor = 'blue';
+        },
+
+        textColor(event){
+          console.log(event)
+          event.target.style.color = 'pink';
+        }
+    },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <FunComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import FunComponent from '@/components/FunComponent.vue'
+export default {
+  name: 'HomeView',
+  components: {
+  FunComponent
+  }
+}
+</script>
